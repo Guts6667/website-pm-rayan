e3dfdb0719c122412e96920f48fcc474b553ae3f
Rayan <rayanchambet1@gmail.com>
2024-12-05 14:20:36 +0100

    New Attempt

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,35 +1,49 @@
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import { notFound } from "next/navigation";
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, setRequestLocale } from 'next-intl/server';
+import { notFound } from 'next/navigation';
 import { routing } from "../../../i18n/routing";
 import StickyMenu from "../components/(nav)/StickyMenu";
 import "./globals.css";
 import { ThemeProvider } from "../../utils/themeContext";
-import Cursor from "../components/Cursor";
+import Cursor from '../components/Cursor';
 
 export default async function LocaleLayout({ children, params }) {
   const { locale } = await params;
-  // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale)) {
     notFound();
   }
-  // Providing all messages to the client
-  // side is the easiest way to get started
   const messages = await getMessages();
 
   return (
     <html lang={locale}>
-      <meta name="color-scheme" content="light dark" />
-      <meta
-        name="theme-color"
-        content="#FCF6E6"
-        media="(prefers-color-scheme: light)"
-      />
-      <meta
-        name="theme-color"
-        content="#141310"
-        media="(prefers-color-scheme: dark)"
-      />
+      <head>
+        <meta name="color-scheme" content="light dark" />
+        <meta
+          name="theme-color"
+          content="#FCF6E6"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#141310"
+          media="(prefers-color-scheme: dark)"
+        />
+        <script>
+          {`
+            const updateMetaThemeColor = () => {
+              const themeColorMetaTag = document.querySelector('meta[name="theme-color"]');
+              if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                themeColorMetaTag.setAttribute('content', '#141310');
+              } else {
+                themeColorMetaTag.setAttribute('content', '#FCF6E6');
+              }
+            };
+
+            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateMetaThemeColor);
+            updateMetaThemeColor();
+          `}
+        </script>
+      </head>
       <body className="bg-white dark:bg-black">
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider>
