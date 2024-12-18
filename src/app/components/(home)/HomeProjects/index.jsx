709ddb6c811e01bd009c3626e9a754cf690f5ee3
Rayan <rayanchambet1@gmail.com>
2024-12-18 12:05:40 +0100

    Few small updates
    Need to write the texts now

--- src/app/components/(home)/HomeProjects/index.jsx
+++ src/app/components/(home)/HomeProjects/index.jsx
@@ -5,26 +5,25 @@ import projectCardEn from "/public/projects/en/project-en.json";
 import projectCardFr from "/public/projects/fr/project-fr.json";
 import SectionHead from "../SectionHead";
 import CardStack from "../CardStack";
-export default function HomeProjects({locale, content}) {
+export default function HomeProjects({ locale, content }) {
   // Let's verify the locale to determine which project card to display
-const projectCard = locale === "en" ? projectCardEn : projectCardFr;
-  
+  const projectCard = locale === "en" ? projectCardEn : projectCardFr;
+
   return (
     <section className=" px-[24px] lg:px-[48px] relative ">
-        <SectionHead style={""} title={content.title} tagline={content.tagline} link="/projects" buttonText={content.buttonText} />
-    {/* <div>
-    {projectCard.map((project, index) => (
-        <Card key={index} project={project} />
-      ))}
-    </div> */}
-    <div >
-      {projectCard.map((project, i) => (
-        <CardStack key={`p_${i}`} project={project} i={i} />
-      ))}
-    </div>
-       
+      <SectionHead
+        style={""}
+        title={content.title}
+        tagline={content.tagline}
+        link="/projects"
+        buttonText={content.buttonText}
+      />
 
-     
+      <div>
+        {projectCard.slice(0, 5).map((project, i) => (
+          <CardStack key={`p_${i}`} project={project} i={i} />
+        ))}
+      </div>
     </section>
   );
 }
