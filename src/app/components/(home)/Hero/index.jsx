5a76d5f7d2a8194f502881e6d2280431a9a1c5e4
Rayan <rayanchambet1@gmail.com>
2024-12-24 08:28:02 +0100

    Fixing hero

--- src/app/components/(home)/Hero/index.jsx
+++ src/app/components/(home)/Hero/index.jsx
@@ -12,7 +12,7 @@ export default function Hero( ) {
 
   return (
     <section className={`light:bg-secondary dark:bg-primary min-h-[100vh] h-fit flex flex-col justify-center items-center gap-[6px] px-[24px] py-[48px] overflow-hidden z-10`}>
-      <div className="relative w-full max-w-[500px] aspect-[4/3] ">
+      <div className="relative w-full max-w-[400px] aspect-[4/3] ">
         <HeroImg src="/img/picture-rayan.png" alt="Rayan" />
         <p className="text-primary light:text-secondary text-center absolute end-1.5">{t("heroText")}</p>
       </div>
